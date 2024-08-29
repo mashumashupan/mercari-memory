535555fa464770489b284ed3ef6c8afc163af1c1
haruna <kharuna0404@gmail.com>
2024-08-29 15:02:49 -0700

    useEffectにconsole.log追加

--- frontend/src/app/chat-2/page.tsx
+++ frontend/src/app/chat-2/page.tsx
@@ -53,6 +53,7 @@ export default function ChatPage() {
     return () => {
       // セッションストレージから画像データを取得
       const imageSrc = sessionStorage.getItem('capturedImage');
+      console.log('imageSrc:', imageSrc);
       if (imageSrc) {
         setCapturedImage(imageSrc);
         startChat(imageSrc, existingSessionId); // 画像を送信
